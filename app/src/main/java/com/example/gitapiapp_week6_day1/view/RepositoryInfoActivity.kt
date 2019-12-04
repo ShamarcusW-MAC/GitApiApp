ac895a36d13944a9099fbb7028185b4cf6b553db
ShamarcusW-MAC <56922234+ShamarcusW-MAC@users.noreply.github.com>
2019-12-04 12:45:07 -0500

    Added repository web page

--- app/src/main/java/com/example/gitapiapp_week6_day1/view/RepositoryInfoActivity.kt
+++ app/src/main/java/com/example/gitapiapp_week6_day1/view/RepositoryInfoActivity.kt
@@ -1,7 +1,10 @@
 package com.example.gitapiapp_week6_day1.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.webkit.WebView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.gitapiapp_week6_day1.R
@@ -18,9 +21,19 @@ class RepositoryInfoActivity : AppCompatActivity() {
         val repoName = findViewById<TextView>(R.id.repo_name_textview)
         val repoUrl = findViewById<TextView>(R.id.repo_url_textview)
 
+//        val repoUrl = findViewById<WebView>(R.id.repo_url_textview)
+
         val avatarUrl: String = intent.getStringExtra("avatar")
         repoName.text = intent.getStringExtra("name")
         repoUrl.text = intent.getStringExtra("url")
+//        repoUrl.loadUrl(intent.getStringExtra("url"))
+
+        repoUrl.setOnClickListener {
+            val intent = Intent(this, RepositoryWeb::class.java)
+            intent.putExtra("url",repoUrl.text.toString())
+            startActivity(intent)
+        }
+//        repoUrl.movementMethod(LinkMovementMethod.getInstance())
 
         Glide.with(this)
             .load(avatarUrl)
---
+++ app/src/main/java/com/example/gitapiapp_week6_day1/view/RepositoryWeb.kt
@@ -0,0 +1,22 @@
+package com.example.gitapiapp_week6_day1.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import com.example.gitapiapp_week6_day1.R
+
+class RepositoryWeb : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_web)
+
+        val intent = intent
+//        intent.getStringExtra("url")
+
+        val repoWeb = findViewById<WebView>(R.id.repository_webview)
+
+        repoWeb.loadUrl(intent.getStringExtra("url")
+        )
+    }
+}
